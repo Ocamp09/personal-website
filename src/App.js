cad193fde54bc5625288313025617a6c9b33de11
Owen campbell <campbeo2@miamioh.edu>
2022-11-03 14:48:20 -0400

    add in different themes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import useLocalStorage from 'use-local-storage';
+import Home from './pages/home';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark);
+  const themes = ['light', 'dark', 'orchid', 'lemon-lime'];
+
+  const switchTheme = () => {
+    let currInd = 0;
+
+    for (let i = 0; i < themes.length; i++) {
+      if (themes[i] === theme) {
+        currInd = i;
+        break;
+      }
+    }
+
+    if (currInd === themes.length - 1) {
+      currInd = 0;
+    } else {
+      currInd++;
+    }
+    const newTheme = themes[currInd];
+    setTheme(newTheme);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App deleted" data-theme={theme}>
+      <button onClick={switchTheme}>
+        Try a new theme
+      </button>
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+
+    return(
+        <div>
+            <h1>
+                OWEN CAMPBELL
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
