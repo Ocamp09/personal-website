4a83deaa6b944e7dbea37bee18ac2db402c729db
Ocamp09 <owen19campbell@gmail.com>
2025-01-23 21:49:31 -0500

    add project grid layout

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -7,8 +7,18 @@ function App() {
   return (
     <>
       <Header></Header>
-      <About></About>
-      <Project></Project>
+      <div className="spacer"></div>
+      <div className="body">
+        <h3 className="card">About me - </h3>
+        <About></About>
+        <h3 className="card">Projects - </h3>
+        <div className="projects">
+          <Project></Project>
+          <Project></Project>
+          <Project></Project>
+          <Project></Project>
+        </div>
+      </div>
       {/* <About></About>
       <About></About>
       <About></About> */}
--- personal-website/src/components/Project.tsx
+++ personal-website/src/components/Project.tsx
@@ -4,14 +4,14 @@ const Project = () => {
   return (
     <>
       <div className="project">
-        <p>Last updated: 2025</p>
-        <p>This Site!</p>
+        <h3>This Site!</h3>
         <p>The source of this website</p>
         <div className="project-links">
-          <button className="buttons">Bio</button>
+          <button className="buttons">About</button>
           <button className="buttons">Demo</button>
           <button className="buttons">Source</button>
         </div>
+        <p className="date">Last updated: 2025</p>
       </div>
     </>
   );
