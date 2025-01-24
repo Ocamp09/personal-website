236979a73fd48108cd7183b92879280ad15e33df
Ocamp09 <campbeo2@miamioh.edu>
2025-01-24 16:17:49 -0500

    add local json

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,8 +2,39 @@ import "./App.css";
 import About from "./components/About";
 import Header from "./components/Header";
 import Project from "./components/Project";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface Project {
+  name: string;
+  repo: string;
+}
+
+interface RootObject {
+  projects: Project[];
+}
 
 function App() {
+  const [projects, setProjects] = useState<RootObject>({
+    projects: [],
+  });
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/projects`);
+        console.log("axios", response.data);
+        setProjects(response.data);
+      } catch (err) {
+        console.error("Error fetching projects", err);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  console.log(projects);
+
   return (
     <>
       <Header></Header>
@@ -13,10 +44,9 @@ function App() {
         <About></About>
         <h3 className="card">Projects - </h3>
         <div className="projects">
-          <Project></Project>
-          <Project></Project>
-          <Project></Project>
-          <Project></Project>
+          {/* {projects.project s.map((project) => (
+            <Project key={project.name} project={project} />
+          ))} */}
         </div>
       </div>
       {/* <About></About>
--- personal-website/src/components/Project.tsx
+++ personal-website/src/components/Project.tsx
@@ -1,9 +1,19 @@
 import "./Project.css";
+import React from "react";
 
-const Project = () => {
+interface ProjectProps {
+  key: string;
+  project: {
+    name: string;
+    repo: string;
+  };
+}
+
+const Project: React.FC<ProjectProps> = ({ key, project }) => {
+  console.log("proj prop", project);
   return (
     <>
-      <div className="project">
+      <div className="project" key={key}>
         <h3>This Site!</h3>
         <p>The source of this website</p>
         <div className="project-links">
