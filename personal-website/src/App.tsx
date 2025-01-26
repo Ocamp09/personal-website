100fd2d654a3225a8a507ca48151227b15b6a63f
Ocamp09 <owen19campbell@gmail.com>
2025-01-25 23:19:10 -0500

    render project info from json server

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -8,22 +8,16 @@ import { useState, useEffect } from "react";
 interface Project {
   name: string;
   repo: string;
-}
-
-interface RootObject {
-  projects: Project[];
+  description: string;
 }
 
 function App() {
-  const [projects, setProjects] = useState<RootObject>({
-    projects: [],
-  });
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
         const response = await axios.get(`http://localhost:3000/projects`);
-        console.log("axios", response.data);
         setProjects(response.data);
       } catch (err) {
         console.error("Error fetching projects", err);
@@ -33,25 +27,20 @@ function App() {
     fetchProjects();
   }, []);
 
-  console.log(projects);
-
   return (
     <>
       <Header></Header>
       <div className="spacer"></div>
       <div className="body">
-        <h3 className="card">About me - </h3>
+        <h3 className="card">- About me - </h3>
         <About></About>
-        <h3 className="card">Projects - </h3>
+        <h3 className="card">- Projects - </h3>
         <div className="projects">
-          {/* {projects.project s.map((project) => (
-            <Project key={project.name} project={project} />
-          ))} */}
+          {projects.map((project) => (
+            <Project key={project} project={project} />
+          ))}
         </div>
       </div>
-      {/* <About></About>
-      <About></About>
-      <About></About> */}
     </>
   );
 }
--- personal-website/src/components/Project.tsx
+++ personal-website/src/components/Project.tsx
@@ -6,20 +6,28 @@ interface ProjectProps {
   project: {
     name: string;
     repo: string;
+    description: string;
   };
 }
 
 const Project: React.FC<ProjectProps> = ({ key, project }) => {
-  console.log("proj prop", project);
+  console.log("proj prop", project.name);
   return (
     <>
       <div className="project" key={key}>
-        <h3>This Site!</h3>
-        <p>The source of this website</p>
+        <h2 className="name">{project.name}</h2>
+        <p>{project.description}</p>
         <div className="project-links">
           <button className="buttons">About</button>
           <button className="buttons">Demo</button>
-          <button className="buttons">Source</button>
+          <button
+            className="buttons"
+            onClick={() => {
+              window.open(project.repo);
+            }}
+          >
+            Source
+          </button>
         </div>
         <p className="date">Last updated: 2025</p>
       </div>
