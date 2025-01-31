f31e626f63421815c4df95c931bf6de9f512bcf7
Ocamp09 <owen19campbell@gmail.com>
2025-01-31 09:29:49 -0500

    hard code projects for no-backend approach

--- personal-website/src/components/Project.tsx
+++ personal-website/src/components/Project.tsx
@@ -2,108 +2,105 @@ import "./Project.css";
 import React from "react";
 
 interface ProjectProps {
-  key: string;
+  key: number;
   project: {
     name: string;
-    repo: string;
+    repo?: string;
     description: string;
     updated: string;
-    publication: string;
-    about: string;
-    demo: string;
+    publication?: string;
+    about?: string;
+    demo?: string;
   };
 }
 
 interface ButtonProps {
-  item: string;
+  item?: string;
 }
 
 const Repo: React.FC<ButtonProps> = ({ item }) => {
-  if (item != undefined || item == "") {
+  if (item) {
     return (
-      <>
-        <button
-          className="buttons"
-          onClick={() => {
-            window.open(item);
-          }}
-        >
-          Repo
-        </button>
-      </>
+      <button
+        className="buttons"
+        onClick={() => {
+          window.open(item);
+        }}
+      >
+        Repo
+      </button>
     );
-  } else return <></>;
+  }
+  return null;
 };
+
 const Publication: React.FC<ButtonProps> = ({ item }) => {
-  if (item != undefined || item == "") {
+  if (item) {
     return (
-      <>
-        <button
-          className="buttons"
-          onClick={() => {
-            window.open(item);
-          }}
-        >
-          Publication
-        </button>
-      </>
+      <button
+        className="buttons"
+        onClick={() => {
+          window.open(item);
+        }}
+      >
+        Publication
+      </button>
     );
-  } else return <></>;
+  }
+  return null;
 };
+
 const About: React.FC<ButtonProps> = ({ item }) => {
-  if (item != undefined || item == "") {
+  if (item) {
     return (
-      <>
-        <button
-          className="buttons"
-          onClick={() => {
-            window.open(item);
-          }}
-        >
-          About
-        </button>
-      </>
+      <button
+        className="buttons"
+        onClick={() => {
+          window.open(item);
+        }}
+      >
+        About
+      </button>
     );
-  } else return <></>;
+  }
+  return null;
 };
+
 const Demo: React.FC<ButtonProps> = ({ item }) => {
-  if (item != undefined || item == "") {
+  if (item) {
     return (
-      <>
-        <button
-          className="buttons"
-          onClick={() => {
-            window.open(item);
-          }}
-        >
-          Demo
-        </button>
-      </>
+      <button
+        className="buttons"
+        onClick={() => {
+          window.open(item);
+        }}
+      >
+        Demo
+      </button>
     );
-  } else return <></>;
+  }
+  return null;
 };
 
 const Project: React.FC<ProjectProps> = ({ key, project }) => {
-  if (project.description == undefined) {
+  if (!project.description) {
     project.description = "See the item's README for more info";
   }
 
   return (
-    <>
-      <div className="project" key={key}>
-        <div className="name-div">
-          <h2 className="name">{project.name}</h2>
-        </div>
-        <p className="description">{project.description}</p>
-        <div className="project-links">
-          <About item={project.about}></About>
-          <Demo item={project.demo}></Demo>
-          <Repo item={project.repo}></Repo>
-          <Publication item={project.publication}></Publication>
-        </div>
-        <p className="date">Last updated: {project.updated}</p>
+    <div className="project" key={key}>
+      <div className="name-div">
+        <h2 className="name">{project.name}</h2>
       </div>
-    </>
+      <p className="description">{project.description}</p>
+      <div className="project-links">
+        <About item={project.about} />
+        <Demo item={project.demo} />
+        <Repo item={project.repo} />
+        <Publication item={project.publication} />
+      </div>
+      <p className="date">Last updated: {project.updated}</p>
+    </div>
   );
 };
 
